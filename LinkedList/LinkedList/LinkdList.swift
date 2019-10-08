b48db737003daea382183b0b2029a1c99ae01c95
Sirarpi Bayramyan <Sirarpi1997@gmail.com>
2019-10-08 23:24:36 +0400

    Added LinkedList file

---
+++ LinkedList/LinkedList/LinkdList.swift
@@ -0,0 +1,222 @@
+//
+//  LinkdList.swift
+//  LinkedList
+//
+//  Created by Sirarpi on 10/8/19.
+//  Copyright © 2019 Sirarpi. All rights reserved.
+//
+
+import Foundation
+
+public  final class  LinkedList<Element> {
+    
+    //LinkedList's Node Class Declaration
+   public class LinkedListNode<Element> {
+        var value: Element
+        var next:  LinkedListNode?
+        weak var previous: LinkedListNode?
+        
+        public init(value: Element) {
+            self.value = value
+        }
+    }
+
+    //Typealiasing the node classto increase readability of code
+    public typealias Node = LinkedListNode<Element>
+    
+    //The head of the LinkedList
+    private(set) var head: Node?
+    
+    //Computed property to iterate through the LinkedList and return the last node (if any)
+    public var last: Node? {
+        guard var node = head else {
+            return nil
+        }
+        while let next = node.next {
+            node = next
+        }
+        return node
+    }
+    
+    //Computed property to check if the LinkedList is empty
+    public var IsEmpty: Bool {
+        return head == nil
+    }
+    
+    //Computed property to iterate through the linked list and return the total number of nodes
+    public var count: Int {
+        guard var node = head else {
+            return 0
+        }
+        var count = 1
+        while let next = node.next {
+            node = next
+            count += 1
+        }
+        return count
+    }
+    
+    //Default initializer
+    public init() {}
+    
+    //Subscript function to return the node at a specific index
+    public subscript(index: Int) -> Element {
+        let node = self.node(at: index)
+        return node.value
+    }
+    
+    //Function to return the node at a specific index. Crashes if index is out of bounds (0...self.count)
+    public func node(at index: Int) -> Node {
+        assert(head != nil, "List is empty")
+        assert(index >= 0, "index must be greater or equal to 0")
+        
+        if index == 0 {
+            return head!
+        } else {
+            var node = head!.next
+            for _ in 1..<index {
+                node = node?.next
+                if node == nil {
+                    break
+                }
+            }
+            assert(node != nil, "index out of bounds")
+            return node!
+        }
+    }
+    
+    // Append a value to the end of the list
+    public func append(_ value: Element) {
+        let newNode = Node(value: value)
+        append(newNode)
+    }
+    
+   // Append a copy of a LinkedListNode to the end of the list
+    public func append(_ node: Node){
+        let newNode = node
+        if let lastNode = last {
+            newNode.previous = lastNode
+            lastNode.next = newNode
+        } else {
+            head = newNode
+        }
+    }
+    
+    // Append a copy of a LinkedList to the end of the list
+    public func append(_ list: LinkedList) {
+        var nodeToCopy = list.head
+        while let node = nodeToCopy {
+            append(node.value)
+            nodeToCopy = node.next
+        }
+    }
+    
+    // Insert a value at a specific index. Crashes if index is out of bounds (0...self.count)
+    public func insert(_ value: Element, at index: Int) {
+        let newNode = Node(value: value)
+        insert(newNode, at: index)
+    }
+    
+    // Insert a copy of a node at a specific index. Crashes if index is out of bounds (0...self.count)
+    public func insert(_ newNode: Node, at index: Int) {
+        if index == 0 {
+            newNode.next = head
+            head?.previous = newNode
+            head = newNode
+        } else {
+            let prev = node(at: index - 1)
+            let next = prev.next
+            newNode.next = prev
+            newNode.next = next
+            next?.previous = newNode
+            prev.next = newNode
+        }
+    }
+    
+    // Insert a copy of a LinkedList at a specific index. Crashes if index is out of bounds (0...self.count)
+    public func insert(_ list : LinkedList, at index: Int){
+        guard !list.IsEmpty else { return }
+        
+        if index == 0 {
+            list.last?.next = head
+            head = list.head
+        } else {
+            let prev = node(at: index - 1)
+            let next = prev.next
+            
+            prev.next = list.head
+            list.head?.previous = prev
+            list.last?.next = next
+            next?.previous = list.last
+        }
+    }
+    
+    // Function to remove all nodes/value from the list
+    public func removeAll() {
+        head = nil
+    }
+     
+    // Function to remove a specific node
+    public func remove(node: Node) -> Element {
+        let prev = node.previous
+        let next = node.next
+        
+        if let prev = prev {
+            prev.next  = next
+        } else {
+            head = next
+        }
+        next?.previous = prev
+        node.next = nil
+        
+        return node.value
+    }
+    
+    // Function to remove the last node/value in the list. Crashes if the list is empty
+    public func removeLast() -> Element {
+        assert(!IsEmpty)
+        return remove(node: last!)
+    }
+    
+    // Function to remove a node/value at a specific index. Crashes if index is out of bounds (0...self.count)
+    public func remove(at index: Int) -> Element {
+        let node = self.node(at: index)
+        return remove(node: node)
+    }
+}
+
+// Extension to enable the standard conversion of a list to String
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        var string = "["
+        var node = head
+        while let nd = node {
+            string += "\(nd.value)"
+            node = nd.next
+            if node != nil {
+                string += ", "
+            }
+        }
+            return string + "]"
+    }
+}
+
+//Extension to enable initialization from an Array
+extension LinkedList {
+    convenience init(array: Array<Element>) {
+        self.init()
+        array.forEach { append($0) }
+    }
+}
+
+//Extension to enable initialization from an Array Literal
+extension LinkedList: ExpressibleByArrayLiteral {
+
+    public convenience init(arrayLiteral elements: Element...){
+        self.init()
+        elements.forEach{
+            append($0)
+        }
+    }
+}
+//Collection
---
+++ LinkedList/LinkedList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LinkedList
+//
+//  Created by Sirarpi on 10/8/19.
+//  Copyright © 2019 Sirarpi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
